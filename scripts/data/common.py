8635288ea6d365c2b22d98f85dbe5655bf789725
GeniusGaryant <1546308416@qq.com>
2019-05-07 16:49:28 +0800

    fix a bug

--- scripts/data/common.py
+++ scripts/data/common.py
@@ -49,7 +49,7 @@ def set_channel(*args, n_channels=3):
 def np2Tensor(*args, rgb_range=255):
     def _np2Tensor(img):
         np_transpose = np.ascontiguousarray(img.transpose((2, 0, 1)))
-        tensor = torch.from_numpy(np_transpose).float()
+        tensor = torch.from_numpy(np_transpose.copy()).float()
         tensor.mul_(rgb_range / 255)
 
         return tensor
