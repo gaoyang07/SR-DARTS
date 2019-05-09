ad402083c7363cb6c1bd6be8271841aadff806e7
GeniusGaryant <1546308416@qq.com>
2019-05-09 16:28:12 +0800

    remove an unused searching unit

--- scripts/model/operations.py
+++ scripts/model/operations.py
@@ -18,17 +18,6 @@ OPS = {
 }
 
 
-class BaseConv(nn.Module):
-
-    def __init__(self, C_in, C_out, kernel_size, stride, padding, affine=True):
-        super(BaseConv, self).__init__()
-        self.op = nn.Sequential(
-            nn.Conv2d(C_in, C_out, kernel_size=kernel_size, stride=stride,
-                      padding=padding, bias=False),
-        )
-
-
-
 class ReLUConvBN(nn.Module):
 
     def __init__(self, C_in, C_out, kernel_size, stride, padding, affine=True):
